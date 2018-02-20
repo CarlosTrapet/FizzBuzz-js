96f05c2eccefcc084324284e761813afb7372eb8
carlostrapet <trapet_llamas@hotmail.com>
2018-02-20 14:43:00 +0000

    initial commit. added basic tests. all passing

---
+++ src/fizzBuzz.js
@@ -0,0 +1,18 @@
+
+function Fizzbuzz() {
+
+};
+
+Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
+return(number % divisor === 0);
+};
+
+Fizzbuzz.prototype.isDivisibleByThree = function(number) {
+  return this._isDivisibleBy(number, 3);
+};
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+};
+Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+};
