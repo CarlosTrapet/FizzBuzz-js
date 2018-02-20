e3f00d41b651dafda5e6dbbe5902a869fde854f0
carlostrapet <trapet_llamas@hotmail.com>
2018-02-20 15:03:48 +0000

    finished fizzbuzz. created for loop

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -16,3 +16,25 @@ Fizzbuzz.prototype.isDivisibleByFive = function(number) {
 Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
   return this._isDivisibleBy(number, 15);
 };
+
+Fizzbuzz.prototype.says = function(number) {
+  if(this.isDivisibleByFifteen(number)) {
+    return "FizzBuzz";
+  } else if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  } else if (this.isDivisibleByThree(number)) {
+    return "Fizz";
+  } else {
+    return number
+  };
+};
+
+fizzbuzz = new Fizzbuzz();
+
+function spitOut() {
+  for(let i = 1; i <= 30; i++){
+    console.log(fizzbuzz.says(i));
+  };
+};
+
+spitOut()
